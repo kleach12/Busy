44d832fab31002a42b14486331667b66489c4be1
Kale Leach <kleach1212@gmail.com>
2023-05-22 14:58:36 -0700

    Commit 4: Added Map need to fix runtime error

--- client/src/Home/Home.js
+++ client/src/Home/Home.js
@@ -1,8 +1,24 @@
+import './Home.css'
 
-export default function Home(){
-  return(
-    <div>
-      <h2> Hey </h2>
+import { useJsApiLoader, GoogleMap } from "@react-google-maps/api";
+const center = { lat: 32.715736 , lng: -117.161087 };
+
+export default function Home() {
+  const isLoaded = useJsApiLoader({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+  });
+
+  if (!isLoaded) {
+    return (<h2>...loading</h2>)
+  }
+
+  return (
+    <div className="flex">
+      <GoogleMap
+        center={center}
+        zoom={15}
+        mapContainerStyle={{ width: "100%", height: "100%" }}
+      ></GoogleMap>
     </div>
-  )
+  );
 }

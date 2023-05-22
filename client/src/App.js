ce153dde92dfe23f5e623a367aee1512a949b357
Kale Leach <kleach1212@gmail.com>
2023-05-22 13:50:03 -0700

    Commit 3: Setting up Home

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import {useEffect} from "react"
-
+import Home from './Home/Home';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
 
@@ -13,20 +14,9 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ client/src/Home/Home.js
@@ -0,0 +1,8 @@
+
+export default function Home(){
+  return(
+    <div>
+      <h2> Hey </h2>
+    </div>
+  )
+}

32c7cfbf16fc5cf68f301e963787b1134726716d
Kale Leach <kleach1212@gmail.com>
2023-05-22 20:24:45 -0700

    Commit 8: Fixed issue with google map

--- client/src/Home/Home.js
+++ client/src/Home/Home.js
@@ -1,17 +1,16 @@
-import './Home.css'
+import "./Home.css";
 
 import { useJsApiLoader, GoogleMap } from "@react-google-maps/api";
-const center = { lat: 32.715736 , lng: -117.161087 };
+const center = { lat: 32.715736, lng: -117.161087 };
 
 export default function Home() {
-  const isLoaded = useJsApiLoader({
+  const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
   });
 
-  if (!isLoaded) {
-    return (<h2>...loading</h2>)
-  }
-
+ 
+  if (!isLoaded) return <h2> Loading...</h2>
+  
   return (
     <div className="flex">
       <GoogleMap
